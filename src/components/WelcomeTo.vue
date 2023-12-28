296fe2521467a801152910e99f54435954c7d59c
Long Yi kang <75409125+gods-fall@users.noreply.github.com>
2023-12-28 13:05:06 +0800

    theSph1305

---
+++ src/components/WelcomeTo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>404</div>
+</template>
+
+<script>
+import WelcomeItemVue from './WelcomeItem.vue'
+export default {
+      name:'WelcomeItemVue',
+      components: {
+        WelcomeItemVue
+    }
+}
+</script>
+
+<style>
+
+</style>
